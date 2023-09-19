acf22ad9197e955e46671ad6e908be8bdcf84d48
kevin george mathew <kevgmat@gmail.com>
2023-09-19 23:09:52 +0530

    EMA & SMa

--- stock_analyser.py
+++ stock_analyser.py
@@ -1,26 +1,167 @@
 import os.path
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 dataset_names = []
 dataset_data = []
 folder_path = 'dataset'
-file_size = 0
+size = 0
 
 if os.path.exists(folder_path):
     files = os.listdir(folder_path)
     for file in files:
         if os.path.isfile(os.path.join(folder_path, file)):
-            df = pd.read_csv('dataset/'+ file)
-            file = file[0:-7]
-            dataset_names.append(file)
-            dataset_data.append(df)
-            file_size = file_size+1
+            if file.endswith('NS.csv'):
+
+                #file = "dataset/" + file
+                #print(file)
+                df = pd.read_csv('dataset/'+file)
+                file = file[0:-7]
+                dataset_names.append(file)
+                dataset_data.append(df)
+                size = size+1
 else:
-    print("the folder ", folder_path," does not exist")
+    print("the folder ", folder_path, " does not exist")
 
 dataset_dict = {}
 for name, value in zip(dataset_names, dataset_data):
-    dataset_dict[name] = value
+    dataset_dict[name] = [[value]]
+# print(dataset_dict)
+
+# HISTORICAL PRICE PERFORMANCE ##############################################################
+# print(dataset_dict)
+for i in range(0, size):
+    final_acp = dataset_data[i]['Adj Close'].iloc[-1]
+    initial_acp = dataset_data[i]['Adj Close'].iloc[0]
+    annual_return = '{:.3f}'.format(((final_acp - initial_acp)/initial_acp)*100)
+    # dataset_dict[dataset_names[i]][0](annual_return)
+    (dataset_dict[dataset_names[i]]).append(annual_return)
+
+for i in range(0, size):
+    print((dataset_dict[dataset_names[i]])[1])
+# for i in range(0, size):
+#     print(dataset_dict[dataset_names[i]])
+# print(dataset_dict)
+# print(dataset_names)
+
+# test = dataset_names[:]
+#
+# for i in range(0, size):
+#     swapped = False
+#     for j in range(0, size- i - 1):
+#         if (dataset_dict[dataset_names[i]])[1] > (dataset_dict[dataset_names[i+1]])[1]:
+#             test[j], test[j+1] = test[j+1], test[j]
+#             swapped = True
+#         if not swapped:
+#             break
+# # print(test)
+# for i in range(0, size):
+#     print(test[i])
+
+# MOVING AVERAGES (SMA & EMA)###################################################
+
+def SMA_EMA(df_A_name, df_B_name):
+
+    df_A = pd.read_csv('dataset/'+ df_A_name + '.NS.csv')
+    df_B = pd.read_csv('dataset/'+ df_B_name + '.NS.csv')
+
+    # Define moving average window sizes
+    short_window = 50  # Short-term window
+    long_window = 200  # Long-term window
+
+    # Calculate SMA and EMA for Stock A
+    df_A['SMA_A'] = df_A['Adj Close'].rolling(window=short_window).mean()
+    df_A['EMA_A'] = df_A['Adj Close'].ewm(span=short_window, adjust=False).mean()
+
+    # Calculate SMA and EMA for Stock B
+    df_B['SMA_B'] = df_B['Adj Close'].rolling(window=short_window).mean()
+    df_B['EMA_B'] = df_B['Adj Close'].ewm(span=short_window, adjust=False).mean()
+
+    # Calculate the MACD & Signal for Stock A
+    df_A['MACD_A'] = df_A['EMA_A'] - df_A['EMA_A'].rolling(window=long_window).mean()
+    df_A['Signal_A'] = df_A['MACD_A'].ewm(span=9, adjust=False).mean()
+
+    # Calculate the MACD & Signal for Stock B
+    df_B['MACD_B'] = df_B['EMA_B'] - df_B['EMA_B'].rolling(window=long_window).mean()
+    df_B['Signal_B'] = df_B['MACD_B'].ewm(span=9, adjust=False).mean()
+
+    # Calculate Golden Crosses and Death Crosses for Stock A
+    df_A['Golden_Cross_A'] = (df_A['SMA_A'] > df_A['SMA_A'].shift(1)) & (df_B['SMA_B'] < df_B['SMA_B'].shift(1))
+    df_A['Death_Cross_A'] = (df_A['SMA_A'] < df_A['SMA_A'].shift(1)) & (df_B['SMA_B'] > df_B['SMA_B'].shift(1))
+
+    # Calculate Golden Crosses and Death Crosses for Stock B
+    df_B['Golden_Cross_B'] = (df_B['SMA_B'] > df_B['SMA_B'].shift(1)) & (df_A['SMA_A'] < df_A['SMA_A'].shift(1))
+    df_B['Death_Cross_B'] = (df_B['SMA_B'] < df_B['SMA_B'].shift(1)) & (df_A['SMA_A'] > df_A['SMA_A'].shift(1))
+
+    stock_A_metrics = {
+        'Golden_Crosses': df_A['Golden_Cross_A'].sum(),
+        'Death_Crosses': df_A['Death_Cross_A'].sum(),
+        'Last_MACD': df_A['MACD_A'].iloc[-1],
+        'Last_Signal': df_A['Signal_A'].iloc[-1],
+    }
+
+    stock_B_metrics = {
+        'Golden_Crosses': df_B['Golden_Cross_B'].sum(),
+        'Death_Crosses': df_B['Death_Cross_B'].sum(),
+        'Last_MACD': df_B['MACD_B'].iloc[-1],
+        'Last_Signal': df_B['Signal_B'].iloc[-1],
+    }
+
+    def four_inner_factors(stock_A, stock_B):
+        golden_cross_bin = 0
+        death_cross_bin = 0
+        last_MACD_bin = 0
+        last_signal_bin = 0
+
+        if stock_A['Golden_Crosses'] > stock_B['Golden_Crosses']:
+            golden_cross_bin = 1
+        if stock_A['Death_Crosses'] < stock_B['Death_Crosses']:
+            death_cross_bin = 1
+        if stock_A['Last_MACD'] > stock_B['Last_MACD']:
+            last_MACD_bin = 1
+        if stock_A['Last_Signal'] > stock_B['Last_Signal']:
+            last_signal_bin = 1
+
+        stock_valuation = ((golden_cross_bin * 0.2 * (stock_A['Golden_Crosses'] - stock_B['Golden_Crosses']))
+                           + (death_cross_bin * 0.2 * (1 / (stock_A['Death_Crosses'] - stock_B['Death_Crosses'])))
+                           + (last_MACD_bin * 0.3 * (stock_A['Last_MACD'] - stock_B['Last_MACD']))
+                           + (last_signal_bin * 0.3 * (stock_A['Last_Signal'] - stock_B['Last_Signal'])))
+
+        print(golden_cross_bin, death_cross_bin, last_MACD_bin, last_signal_bin)
+        return stock_valuation
+
+    stock_A_valuation = four_inner_factors(stock_A_metrics, stock_B_metrics)
+    stock_B_valuation = four_inner_factors(stock_B_metrics, stock_A_metrics)
+
+    print("A = ", stock_A_valuation)
+    print("B = ", stock_B_valuation)
+    if (stock_A_valuation > stock_B_valuation):
+        return True
+    else:
+        return False
 
 
+SMA_EMA_order = dataset_names[:]
+for i in range(0, size):
+    swapped = False
+    for j in range(0, size - i - 1):
+        df_A_name = SMA_EMA_order[j]
+        df_B_name = SMA_EMA_order[j+1]
+
+        if SMA_EMA(df_A_name, df_B_name):
+            SMA_EMA_order[j], SMA_EMA_order[j+1] = SMA_EMA_order[j+1], SMA_EMA_order[j]
+            swapped = True
+        if not swapped:
+            break
+
+# for i in range(0, size):
+#    print(SMA_EMA_order[i])
+
+for i in SMA_EMA_order:
+    (dataset_dict[i]).append(SMA_EMA_order.index(i))
+
+print(dataset_dict)
+
+#TRADING VOLUMES ##############################################################
 
