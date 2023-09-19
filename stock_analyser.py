737c4aa8158e22bff57b8fd7cc5d134afdf9ead7
kevin george mathew <kevgmat@gmail.com>
2023-09-20 04:18:23 +0530

    stock_valuation_done

--- stock_analyser.py
+++ stock_analyser.py
@@ -37,9 +37,9 @@ for i in range(0, size):
     annual_return = '{:.3f}'.format(((final_acp - initial_acp)/initial_acp)*100)
     # dataset_dict[dataset_names[i]][0](annual_return)
     (dataset_dict[dataset_names[i]]).append(annual_return)
-
-for i in range(0, size):
-    print((dataset_dict[dataset_names[i]])[1])
+#
+# for i in range(0, size):
+# print((dataset_dict[dataset_names[i]])[1])
 # for i in range(0, size):
 #     print(dataset_dict[dataset_names[i]])
 # print(dataset_dict)
@@ -63,8 +63,8 @@ for i in range(0, size):
 
 def SMA_EMA(df_A_name, df_B_name):
 
-    df_A = pd.read_csv('dataset/'+ df_A_name + '.NS.csv')
-    df_B = pd.read_csv('dataset/'+ df_B_name + '.NS.csv')
+    df_A = pd.read_csv('dataset/' + df_A_name + '.NS.csv')
+    df_B = pd.read_csv('dataset/' + df_B_name + '.NS.csv')
 
     # Define moving average window sizes
     short_window = 50  # Short-term window
@@ -128,19 +128,18 @@ def SMA_EMA(df_A_name, df_B_name):
                            + (last_MACD_bin * 0.3 * (stock_A['Last_MACD'] - stock_B['Last_MACD']))
                            + (last_signal_bin * 0.3 * (stock_A['Last_Signal'] - stock_B['Last_Signal'])))
 
-        print(golden_cross_bin, death_cross_bin, last_MACD_bin, last_signal_bin)
+        # print(golden_cross_bin, death_cross_bin, last_MACD_bin, last_signal_bin)
         return stock_valuation
 
     stock_A_valuation = four_inner_factors(stock_A_metrics, stock_B_metrics)
     stock_B_valuation = four_inner_factors(stock_B_metrics, stock_A_metrics)
 
-    print("A = ", stock_A_valuation)
-    print("B = ", stock_B_valuation)
+    # print("A = ", stock_A_valuation)
+    # print("B = ", stock_B_valuation)
     if (stock_A_valuation > stock_B_valuation):
         return True
     else:
         return False
-
 
 SMA_EMA_order = dataset_names[:]
 for i in range(0, size):
@@ -155,13 +154,106 @@ for i in range(0, size):
         if not swapped:
             break
 
-# for i in range(0, size):
-#    print(SMA_EMA_order[i])
-
 for i in SMA_EMA_order:
     (dataset_dict[i]).append(SMA_EMA_order.index(i))
 
-print(dataset_dict)
+# print(dataset_dict)
 
 #TRADING VOLUMES ##############################################################
+
+def OBV(A_file, B_file):
+    df_test_1 = pd.read_csv('dataset/' + A_file +'.NS.csv')
+    df_test_2 = pd.read_csv('dataset/' + B_file +'.NS.csv')
+
+    df_test_1['Daily_OBV_1'] = 0
+    df_test_2['Daily_OBV_2'] = 0
+
+    df_test_1.loc[df_test_1['Adj Close'] > df_test_1['Adj Close'].shift(1), 'Daily_OBV_1'] = df_test_1['Volume']
+    df_test_1.loc[df_test_1['Adj Close'] < df_test_1['Adj Close'].shift(1), 'Daily_OBV_1'] = -df_test_1['Volume']
+    df_test_1['OBV_1'] = df_test_1['Daily_OBV_1'].cumsum()
+
+    df_test_2.loc[df_test_2['Adj Close'] > df_test_2['Adj Close'].shift(1), 'Daily_OBV_2'] = df_test_2['Volume']
+    df_test_2.loc[df_test_2['Adj Close'] < df_test_2['Adj Close'].shift(1), 'Daily_OBV_2'] = -df_test_2['Volume']
+    df_test_2['OBV_2'] = df_test_2['Daily_OBV_2'].cumsum()
+
+    cumulative_obv_1 = df_test_1['OBV_1'].iloc[-1]
+    cumulative_obv_2 = df_test_2['OBV_2'].iloc[-1]
+
+    if cumulative_obv_1 > cumulative_obv_2:
+        return True
+    else:
+        return False
+
+volume_order = dataset_names[:]
+for i in range(0, size):
+    swapped = False
+    for j in range(0, size - i - 1):
+        A_name = volume_order[j]
+        B_name = volume_order[j + 1]
+        if OBV(A_name, B_name):
+            volume_order[j], volume_order[j + 1] = volume_order[j + 1], volume_order[j]
+            swapped = True
+        if not swapped:
+            break
+# for i in range(0, size):
+#     print(volume_order[i])
+for i in volume_order:
+    (dataset_dict[i]).append(volume_order.index(i))
+
+
+##STOCHIASTIC OSCILLATOR ##################################
+
+
+
+def stochastic_oscillator(A_file, B_file):
+
+    df_test_1 = pd.read_csv('dataset/' + A_file +'.NS.csv')
+    df_test_2 = pd.read_csv('dataset/' + B_file +'.NS.csv')
+
+    period = 14
+
+    df_test_1['Lowest_Low_1'] = df_test_1['Low'].rolling(window = period).min()
+    df_test_1['Highest_High_1'] = df_test_1['High'].rolling(window = period).max()
+    df_test_1['%K_stock_1'] = ((df_test_1['Adj Close']))
+
+    df_test_2['Lowest_Low_2'] = df_test_2['Low'].rolling(window=period).min()
+    df_test_2['Highest_High_2'] = df_test_2['High'].rolling(window=period).max()
+    df_test_2['%K_stock_2'] = ((df_test_2['Adj Close']))
+
+    # plt.figure(figsize = (12, 6))
+    # plt.plot(df_test_1['Date'], df_test_1['%K_stock_1'], color = 'blue')
+    # plt.plot(df_test_2['Date'], df_test_2['%K_stock_2'], color = 'green')
+    # plt.xlabel('Date')
+    # plt.ylabel('%K Value')
+    # plt.title('test')
+    # plt.show()
+
+    average_k_1 = df_test_1['%K_stock_1'].mean()
+    average_k_2 = df_test_2['%K_stock_2'].mean()
+
+    if average_k_1 > average_k_2:
+        return True
+    else:
+        return False
+
+stochastic_order = dataset_names[:]
+for i in range(0, size):
+    for j in range(0, size - i - 1):
+        A_name = stochastic_order[j]
+        B_name = stochastic_order[j+1]
+        if stochastic_oscillator(A_name, B_name):
+            stochastic_order[j], stochastic_order[j+1] = stochastic_order[j+1], stochastic_order[j]
+            swapped = True
+        if not swapped:
+            break
+
+# for i in range(0, size):
+#     print(stochastic_order[i])
+for i in volume_order:
+    (dataset_dict[i]).append(stochastic_order.index(i))
+
+
+print(dataset_dict)
+
+
 
